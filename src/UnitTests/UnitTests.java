cb70ea0ca69223cedbdb493f7ac56a812300ebd5
Drew Engberson <drew.engberson@gmail.com>
2018-09-09 17:08:47 -0600

    Created skeleton for unit tests, all tests fail.

---
+++ src/UnitTests/UnitTests.java
@@ -0,0 +1,86 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnitTests {
+
+    @Test
+    public void CanCreateLocals() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanPrintInput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanCreateLiterals() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanAddAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanSubtractAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanMultiplyAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanDivideAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanModAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanEqualsAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanNotEqualsAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanLessThanAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanLTEAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanAndAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanOrAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanNegateAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+    @Test
+    public void CanOppositeAndOutput() {
+        Assert.assertEquals("1", "2");
+    }
+
+}
